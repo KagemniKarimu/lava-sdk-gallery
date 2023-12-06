0c81253393bba2d0a62c84d8d15c1157b40f2eb2
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2023-12-06 17:27:54 -0600

    update custom example / buildExample function

--- examples/custom_ex.js
+++ examples/custom_ex.js
@@ -1,103 +1,205 @@
 import inquirer from 'inquirer';
 import colors from 'colors';
 import { chainList } from '../chains.js';
+import { LavaSDK } from '@lavanet/lava-sdk';
+import ora from 'ora';
 
 export async function buildExample() {
-    const chosenChains = await selectChains()
-    const selectedAPI = await selectApiInterface(chosenChains)
-    const selectedChain = await selectChainForApi(selectedAPI, chosenChains)
-    console.log(selectedChain)
-    const apiCall = await inputApiCall()
+  const selectChainsOutput = await selectChains()
+  const chosenChains = await selectChainsOutput[0];
+  const sdkInstance = await selectChainsOutput[1];
+  const selectedAPI = await selectApiInterface(chosenChains)
+  const selectedChain = await selectChainForApi(selectedAPI, chosenChains)
+  console.log(selectedChain)
+  const apiCall = await inputApiCall(selectedAPI)
 
-    await confirmAndExecute(selectedChain, selectedAPI, apiCall[0])
+  await confirmAndExecute(selectedChain, selectedAPI, apiCall, sdkInstance)
 }
 
 async function selectChains() {
-    console.log("test")
-    const answers = await inquirer.prompt([
+
+  const answers = await inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'selectedChains',
+      message: 'Select chains to make calls on:',
+      choices: chainList.map(chain => ({
+        name: `⛓️ ${chain.chainName.blue} (${chain.chainID.bold})`,
+        value: chain
+      }))
+    }
+  ]);
+
+  const spinner = ora({
+    text: 'Initializing Chains...',
+    spinner: "binary"
+  }).start();
+
+  const sdkInstance = await LavaSDK.create({
+    badge: {
+      badgeServerAddress: "https://badges.lavanet.xyz",
+      projectId: "1f2cecd3bb19f996997560be2184b8d4",
+    },
+    chainIds: answers.selectedChains.map(chain => chain.chainID),
+  })
+
+  spinner.stop();
+
+  return [answers.selectedChains, sdkInstance]
+};
+
+
+async function selectApiInterface(selectedChains) {
+  const apiInterfaces = Array.from(new Set(selectedChains.flatMap(chain => chain.enabledApiInterfaces)));
+
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedApi',
+      message: 'Select an API interface:',
+      choices: apiInterfaces
+    }
+  ]);
+
+  return answers.selectedApi
+}
+
+async function selectChainForApi(selectedApi, chosenChains) {
+  const chains = chosenChains.filter(chain => chain.enabledApiInterfaces.includes(selectedApi));
+  const answers = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'selectedChain',
+      message: 'Select a chain:',
+      choices: chains.map(chain => ({ name: chain.chainName, value: chain.chainID }))
+    }
+  ]);
+
+  return answers.selectedChain;
+}
+
+
+async function inputApiCall(apiInterface) {
+
+  if (apiInterface == "rest") {
+    const connectionInput = await inquirer.prompt([
       {
-        type: 'checkbox',
-        name: 'selectedChains',
-        message: 'Select chains to make calls on:',
-        choices: chainList.map(chain => ({
-          name: `⛓️ ${chain.chainName.blue} (${chain.chainID.bold})`,
-          value: chain
-        }))
+        type: 'input',
+        name: 'connectionType',
+        message: 'Enter the connection type for the API call:',
+        default: "GET"
       }
     ]);
-    return answers.selectedChains
-  };
 
-
-  async function selectApiInterface(selectedChains) {
-    const apiInterfaces = Array.from(new Set(selectedChains.flatMap(chain => chain.enabledApiInterfaces)));
-    
-    const answers = await inquirer.prompt([
+    const urlInput = await inquirer.prompt([
       {
-        type: 'list',
-        name: 'selectedApi',
-        message: 'Select an API interface:',
-        choices: apiInterfaces
+        type: 'input',
+        name: 'endpointURL',
+        message: 'Enter the URL of the Endpoint:',
+        default: "/blocks/latest"
       }
     ]);
 
-    return answers.selectedApi
-  }
+    return [connectionInput.connectionType, urlInput.endpointURL]
 
-  async function selectChainForApi(selectedApi, chosenChains) {
-    const chains = chosenChains.filter(chain => chain.enabledApiInterfaces.includes(selectedApi));
-    const answers = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'selectedChain',
-        message: 'Select a chain:',
-        choices: chains.map(chain => ({ name: chain.chainName, value: chain.chainID }))
-      }
-    ]);
-  
-    return answers.selectedChain;
-  }
-  
+  } else {
 
-async function inputApiCall() {
     const methodInput = await inquirer.prompt([
       {
         type: 'input',
         name: 'methodName',
-        message: 'Enter the method name for the API call:'
+        message: 'Enter the method name for the API call:',
+        default: "eth_blockNumber"
       }
     ]);
 
     const paramsInput = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'parameters',
-      message: 'Input the parameters! (Default: [])'
-    }
+      {
+        type: 'input',
+        name: 'parameters',
+        message: 'Enter the parameters:',
+        default: "[]"
+      }
     ]);
 
-    return [methodInput.methodName, paramsInput.parameters] 
+    return [methodInput.methodName, paramsInput.parameters]
   }
-  
-
-async function confirmAndExecute(selectedChain, selectedApi, methodName) {
-    console.log(`🌋 You have selected to call the method \"${methodName.bold}\" using ${selectedApi.green} on the following chain ${selectedChain.bgWhite}`);
-    // Here you can add logic to execute the mock call or relay the information
-
-    const answer = await inquirer.prompt([
-        {
-            type: 'confirm',
-            name: 'confirmation',
-            message: "Is this the correct API?"
-        }
-    ]);
+}
 
 
-    if (answer.confirmation) {
-        console.log("accept confirmation!");
-    } else if (!answer.confirmation) {
-        console.log("reject confirmation!");
+async function confirmAndExecute(selectedChain, selectedApi, apiCall, sdkInstance) {
+  console.log(`🌋 You have selected to call the method \"${apiCall[0].bold}\" using ${selectedApi.green} on the following chain ${selectedChain.bgWhite}`);
+
+  let relayString;
+
+  if (selectedApi == "rest") {
+    relayString = `
+    sendRelay({       
+      chainId: "${selectedChain}",
+      connectionType: "${apiCall[0]}",
+      url: "${apiCall[1]}",
+      rpcInterface: "${selectedApi}",
+    })`
+
+  } else {
+    relayString = `
+    sendRelay({       
+      chainId: "${selectedChain}",
+      method: "${apiCall[0]}",
+      params: ${apiCall[1]},
+      rpcInterface: "${selectedApi}",
+    })`
+  };
+
+  console.log(relayString);
+
+  const confirmation = await confirmChoices()
+
+  if (confirmation) {
+    console.log("✅ Confirmed. Executing sendRelay...");
+    
+    
+    try {
+    let relayResponse;
+    if (selectedApi == "rest") {
+      relayResponse = await sdkInstance.sendRelay({
+        chainId: selectedChain,
+        connectionType: apiCall[0],
+        url: apiCall[1],
+        rpcInterface: selectedApi,
+      });
     } else {
-        console.log("error");
-    }
+      relayResponse = await sdkInstance.sendRelay({
+        chainId: selectedChain,
+        method: apiCall[0],
+        params: apiCall[1],
+        rpcInterface: selectedApi,
+      });
+    };
+    console.log(relayResponse);
+    
+  } catch (error) {
+    console.error('An error occured:', error);
+  };
+
+
+  } else {
+    console.log("❌ Confirmation declined. Relay not executed.")
   }
+
+};
+
+
+
+async function confirmChoices() {
+
+  const answer = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirmation',
+      message: "Is this the correct API?"
+    }
+  ]);
+
+  return answer.confirmation
+}
