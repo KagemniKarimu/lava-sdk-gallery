cdb01d9ff4085cb0f4cd5dcdd020083497c27e44
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2023-12-08 16:57:49 -0600

    add  new example

--- examples.js
+++ examples.js
@@ -1,7 +1,8 @@
 import inquirer from 'inquirer';
 import colors from 'colors';
-import { buildExample } from './examples/custom_ex.js';
+import { buildExample } from './examples/custom.js';
 import { useMultiChainWithBadges } from './examples/multi-chain-with-badge.js';
+import { getEthAddressBalance } from './examples/eth-address-balance-web3.js';
 
 export async function getExamplesMenu() {
   const answer = await inquirer.prompt([
@@ -38,15 +39,19 @@ async function getPrebuiltExamplesMenu() {
       name: 'action',
       message: 'Prebuilt Examples 🧰:',
       choices: [
-        '⚡🌋 Multi-chain ("ETH1", "FVM", "NEAR", "COSHUB", "STRK", "AXELAR") with Badges',
+        '⚡🌋 Multi-chain Block Height ("ETH1", "FVM", "NEAR", "COSHUB", "STRK", "AXELAR") with Badges',
+        '💰 Get the Balance on an Ethereum Address using Web3.js Integration',
         '🔙 Examples Menu'
       ],
     },
   ]);
 
   switch (answer.action) {
-    case '⚡🌋 Multi-chain ("ETH1", "FVM", "NEAR", "COSHUB", "STRK", "AXELAR") with Badges':
+    case '⚡🌋 Multi-chain Block Height ("ETH1", "FVM", "NEAR", "COSHUB", "STRK", "AXELAR") with Badges':
       await useMultiChainWithBadges()
+      break;
+    case '💰 Get the Balance on an Ethereum Address using Web3.js Integration':
+      await getEthAddressBalance()
       break;
     case '🔙 Examples Menu':
       break;
